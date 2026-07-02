66ec9699bff6f646a1a099cd07055fffe55fffc4
LiiiiPeggy <2400115001@mails.edu.szu.cn>
2026-07-02 17:09:05 +0800

    first commit

---
+++ rs_converter_ws/build/rs_converter/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/work/wyx/lqp/Codes/rs_converter_ws/src/rs_to_velodyne_ros2
+# Build directory: /root/work/wyx/lqp/Codes/rs_converter_ws/build/rs_converter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ rs_converter_ws/build/rs_converter/ament_cmake_symlink_install_targets_0_Release.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/root/work/wyx/lqp/Codes/rs_converter_ws/build/rs_converter/rs_converter" "TARGETS" "rs_converter" "DESTINATION" "lib/rs_converter")
