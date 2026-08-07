f49e4fa47592819fd61ed40203e33cf9ccbdfab7
LiiiiPeggy <2400115001@mails.edu.szu.cn>
2026-08-07 10:27:39 +0800

    feat(repeat): add 5repeat.sh launcher for repeat cruise (grep REPEAT)

---
+++ cmu_planner/5repeat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+
+# loop_count defaults to -1 (infinite); pass a number for fixed round-trips
+loop_count=${1:--1}
+
+ros2 launch vehicle_simulator system_real_robot.launch \
+  enableCruise:=true \
+  rvizWaypointTopic:=/way_point_cruise \
+  repeat_enabled:=true \
+  loop_count:=$loop_count \
+  2>&1 | grep --line-buffered REPEAT
