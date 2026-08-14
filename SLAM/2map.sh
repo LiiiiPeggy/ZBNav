b13c9acc3732c19b4a0b729ab196af056521a2c0
LiiiiPeggy <2400115001@mails.edu.szu.cn>
2026-08-14 14:02:58 +0800

    feat(slam): add 2map.sh launcher for map_downsample

---
+++ SLAM/2map.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/humble/setup.bash
+source install/setup.bash
+
+# ################################
+# Bash: run map_downsample PLY->VoxelGrid->PCD
+# ################################
+# Run from the SLAM workspace root. Usage:
+#   bash 2map.sh [input.ply] [output.pcd] [leaf_size]
+#   defaults: input = prebuilt Odin map PLY, output = input with .ply->.pcd, leaf = 0.05
+input=${1:-src/odin_ros_driver/map/map_20260807_151455.ply}
+
+args=("$input")
+[ -n "$2" ] && args+=("$2")
+[ -n "$3" ] && args+=("$3")
+
+./install/odin_ros_driver/lib/odin_ros_driver/map_downsample "${args[@]}"
